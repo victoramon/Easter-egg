4b4a0dfdfd0c7ff4ee64df5c89b537ac3653a544
DaniaAlmazanSanchez <danniaalmazansanchez@gmail.com>
2021-11-02 22:02:42 -0600

    detalles boton y pop-up

--- src/js/publicaciones.js
+++ src/js/publicaciones.js
@@ -1,20 +1,3 @@
-var btnpublicar = document.getElementById('btnpublicar'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('pop-up'),
-    botoncerrarpopup = document.getElementById('boton-cerrar-pop-up');
-
-
-/**evento boton publicar */
- btnpublicar.addEventListener('click', function(){
-overlay.classList.add('active');
-popup.classList.add('active');
-}); 
-
-/**evento boton cerrar pop-up */
- botoncerrarpopup.addEventListener('click', function(){
-overlay.classList.remove('active');
-popup.classList.remove('active');
-}); 
 
 import {mostrarPublicacion} from './mostrarPublicacion.js';
 import {agregarComentario} from './mostrarPublicacion.js';
